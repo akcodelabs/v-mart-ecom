c11f86b2ac5a59c196592fbdcfcd72caf52edc85
kavi-0909 <pkavi9495@gmail.com>
2025-06-16 17:04:03 +0530

    Revert "Add login/signup modal with validation and toggle (#7)"

--- v-mar-ecom/src/App.jsx
+++ v-mar-ecom/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 
 import './App.css'
-import UserLogin from './components/User'
 
 function App() {
   
 
   return (
-    <div>
-      <UserLogin />
-    </div>
+    <>
+     
+    </>
   )
 }
 
