b83fdca6308959d30d2c893efefb4c67a2b21821
deepak65798 <robik6865@gmail.com>
2025-06-24 11:34:47 +0530

    feat: add @tanstack/react-router dependency and update App component imports

--- v-mar-ecom/src/App.jsx
+++ v-mar-ecom/src/App.jsx
@@ -8,7 +8,8 @@ import Nav from "./components/Nav";
 import ContentSection from "./components/ContentSection";
 import Services from "./components/Services";
 import ServicesExtension from "./components/ServicesExtension";
-
+import Products from "./components/Products";
+import router from "./Components/router";
 function App() {
   return (
     <div className="App">
